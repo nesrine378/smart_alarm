cd5e226c887227ea351a137601a88c95bc0e0449
MalekFAKHFAKH <96119446+MalekFAKHFAKH@users.noreply.github.com>
2022-01-07 14:47:03 +0100

    comparaison

--- iot/main.py
+++ iot/main.py
@@ -11,86 +11,25 @@ from typing import List
 from passlib.context import CryptContext
 import datetime
 from sqlalchemy.sql import select
-import pymongo
-from pymongo import MongoClient
+import psycopg2
 import datetime, uuid
 import time
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+#Connect to MongoDB
 conn = "mongodb+srv://malek:Malek.fakhfakh1@cluster0.ygipb.mongodb.net/SmartAlarmDB?retryWrites=true&w=majority"
 connection = MongoClient(conn)
 
-
 cursor = conn.cursor()
-
-CONFIDENCE_THRESHOLD = 0.4
-NMS_THRESHOLD = 0.3
-COLORS = [(255,0,0),(255,0,255),(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
-GREEN =(0,255,0)
-BLACK =(0,0,0)
-# defining fonts 
-FONTS = cv2.FONT_HERSHEY_COMPLEX
-class_names = []
-with open("classes.txt", "r") as f:
-    class_names = [cname.strip() for cname in f.readlines()]
-#  setttng up opencv net
-yoloNet = cv2.dnn.readNet('yolov5-tiny.weights', 'yolov5-tiny.cfg')
-
-yoloNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-yoloNet.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
-
-model = cv2.dnn_DetectionModel(yoloNet)
-model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
-
-# object detector funciton /method
-def object_detector(image):
-    classes, scores, boxes = model.detect(image, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-    # creating empty list to add objects data
-    data_list =[]
-    for (classid, score, box) in zip(classes, scores, boxes):
-        # define color of each, object based on its class id 
-        color= COLORS[int(classid) % len(COLORS)]
-        print(class_names)
-        print('omar')
-        print(classid)
-        label = "%s : %f" % (class_names[classid], score)
-
-        # draw rectangle on and label on object
-        cv2.rectangle(image, box, color, 2)
-        cv2.putText(image, label, (box[0], box[1]-14), FONTS, 0.5, color, 2)
-    
-        # getting the data 
-        # 1: class name  2: object width in pixels, 3: position where have to draw text(distance)
-        if classid ==0: # person class id 
-            data_list.append([class_names[classid], box[2], (box[0], box[1]-2)])
-        #elif classid ==67:
-           # data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        #elif classid ==2:
-            #data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        #elif classid ==5:
-            #data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        #elif classid ==7:
-            #data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        #elif classid ==3:
-            #data_list.append([class_names[classid[0]], box[2], (box[0], box[1]-2)])
-        # return list 
-    return data_list
 
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    #frame = cv2.imread('img_test/car and bus.png')
-    #frame = cv2.imread('img_test/person.png')
-    #frame = cv2.imread('img_test/Capture.png')
-    #frame = cv.imread('img_test/Capture.png')
-
-
-    #frame = cv.VideoCapture('test.mp4')
-    #frame = cv.imread('img_test/test.mp4')
+  
     ################################################
-    data = object_detector(frame)
-    for d in data:
-        if d[0] =='person':
+    d ='person'
+    for i in range(10):
+        if d =='person':
             ch=""
             start=time.time()
             #traitement de visage
@@ -201,7 +140,9 @@ while True:
             #fin traitement de visage
             print('ALERTE')
             x, y = d[2]
-            cv2.rectangle(frame, (x, y-3), (x+150, y+23),BLACK,-1 )
+            if key ==ord('q'):
+                break
+            #cv2.rectangle(frame, (x, y-3), (x+150, y+23),BLACK,-1 )
     cv2.imshow('frame',frame)
     
     key = cv2.waitKey(1)
@@ -209,5 +150,3 @@ while True:
         break
 cv2.destroyAllWindows()
 cap.release()
-
-
